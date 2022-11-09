5c145bbf557fbb9b202cd2b0c75ec85465ae6614
Anton <bedunkevichaa@gmail.com>
2022-11-09 11:37:34 +0300

    fix: 01-read-file change to ReadStream task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
 
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-)
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
